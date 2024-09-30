bb8f5156ead084f6e31cb59ef235c10ba952f02d
mvelten <markus.velten@tmp.com>
2024-09-30 14:26:02 +0200

    UX improvements

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -7,6 +7,7 @@ import { decode } from '../core/decoder/index.ts';
 import { getLogger } from '../utils/logging/logUtils.ts';
 import process from 'node:process';
 import figlet from 'figlet';
+import inquirer from 'inquirer';
 
 const program = new Command();
 
@@ -23,7 +24,8 @@ program
     .requiredOption('-i, --input <file>', 'Input file to hide')
     .requiredOption('-p, --png-folder <folder>', 'Folder with input PNG files')
     .requiredOption('-o, --output <folder>', 'Output folder to store PNG files')
-    .requiredOption('-w, --password <password>', 'Password for encryption')
+    .requiredOption('-w, --password', 'Prompt for password')
+    .showHelpAfterError()
     .action(async (options) => {
         const debugVisual = program.opts().debugVisual || false;
         const verbose = program.opts().verbose || false;
@@ -31,7 +33,46 @@ program
         const inputFile = path.resolve(options.input);
         const inputPngFolder = path.resolve(options.pngFolder);
         const outputFolder = path.resolve(options.output);
-        const password = options.password;
+
+        let password: string;
+        if (options.password) {
+            const answers = await inquirer.prompt([
+                {
+                    type: 'password',
+                    name: 'password',
+                    message: 'Enter password:',
+                    mask: '*',
+                    validate: (value) => {
+                        if (value.length === 0) {
+                            return 'Password cannot be empty';
+                        }
+                        return true;
+                    },
+                },
+                {
+                    type: 'password',
+                    name: 'confirmPassword',
+                    message: 'Confirm password:',
+                    mask: '*',
+                    validate: (value) => {
+                        if (value.length === 0) {
+                            return 'Password cannot be empty';
+                        }
+                        return true;
+                    },
+                },
+            ]);
+
+            if (answers.password !== answers.confirmPassword) {
+                console.error('Passwords do not match. Please try again.');
+                process.exit(1);
+            }
+
+            password = answers.password;
+        } else {
+            console.error('Password is required');
+            process.exit(1);
+        }
 
         const logger = getLogger('encoder', verbose);
 
@@ -45,6 +86,7 @@ program
                 debugVisual,
                 logger,
             });
+            process.exit(0);
         } catch (error) {
             logger.error(`Encoding failed: ${error}`);
             process.exit(1);
@@ -56,13 +98,36 @@ program
     .description('Decode a file from PNG images')
     .requiredOption('-i, --input <folder>', 'Input folder with PNG files')
     .requiredOption('-o, --output <folder>', 'Output file path')
-    .requiredOption('-w, --password <password>', 'Password for decryption')
+    .requiredOption('-w, --password', 'Prompt for password')
+    .showHelpAfterError()
     .action(async (options) => {
         const verbose = program.opts().verbose || false;
 
         const inputFolder = path.resolve(options.input);
         const outputFolder = path.resolve(options.output);
-        const password = options.password;
+
+        let password: string;
+        if (options.password) {
+            const answers = await inquirer.prompt([
+                {
+                    type: 'password',
+                    name: 'password',
+                    message: 'Enter password:',
+                    mask: '*',
+                    validate: (value) => {
+                        if (value.length === 0) {
+                            return 'Password cannot be empty';
+                        }
+                        return true;
+                    },
+                },
+            ]);
+
+            password = answers.password;
+        } else {
+            console.error('Password is required');
+            process.exit(1);
+        }
 
         const logger = getLogger('decoder', verbose);
 
@@ -74,21 +139,24 @@ program
                 verbose,
                 logger,
             });
+            process.exit(0);
         } catch (error) {
             logger.error(`Decoding failed: ${error}`);
             process.exit(1);
         }
     });
 
-(() => {
+if (import.meta.main) {
+    console.clear();
     console.log(
-        figlet.textSync("Pix-Veil", {
-            font: "DOS Rebel",
-            horizontalLayout: "default",
-            verticalLayout: "default",
+        figlet.textSync('Pix-Veil', {
+            font: 'DOS Rebel',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
             width: 80,
             whitespaceBreak: true,
         })
     );
+    console.log(`A tiny steganography tool to hide secret data into existing png files. (c) 2024, slippyex\n`);
     program.parse(process.argv);
-})();
+}
